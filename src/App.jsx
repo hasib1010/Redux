7d31637c9aeef64e3a79980444a89b1b5186a478
hasib1010 <asifmahmud664@gmail.com>
2024-10-07 22:36:17 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className='text-3xl text-center'>Hi</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
